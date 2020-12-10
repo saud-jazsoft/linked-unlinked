d9d55770cd02d96da1abd6eb67176bb09db7a43a
saud-khalid <70132211+saud-jazsoft@users.noreply.github.com>
2020-12-10 22:14:19 +0500

    Update linked.js

--- linked.js
+++ linked.js
@@ -1,1 +1,8 @@
-// find all the users that have not been linked
+// find all the users that have not been linkedconst args = ['p0', 'p1', 'p2'];
+const arr = ['unlinked', 'linked'];
+arr.apply(this, unlinked);
+
+
+return (
+    console.log(unlinked)
+)
